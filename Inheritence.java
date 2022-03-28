185ed18d5fd3fb5ce203c65f64cd94429de317a9
Kshitij tyagi <kshitijtyagi525@gmail.com>
2022-03-28 08:45:45 +0530

    Add files via upload

---
+++ Inheritence.java
@@ -0,0 +1,42 @@
+package basics;
+class Animal{
+    private String name;
+    Animal(){
+        System.out.println("Inside Animal Constructor");
+        this.name="Animal";
+    }
+    public void Walk(){
+        System.out.println("Animal Walking");
+    }
+
+    public void getAnimalName(){
+        System.out.println("Name : "+this.name);
+    }
+}
+class Dog extends Animal{
+    private String name="Dog";
+    Dog(){
+        //super();
+        System.out.println("Inside Dog Constructor");
+        this.name="Dog";
+    }
+    @Override
+    public void Walk(){
+        super.Walk();
+        System.out.println("Dog Walking");
+    }
+
+    @Override
+    public void getAnimalName(){
+        super.getAnimalName();
+        System.out.println("Name : "+this.name);
+    }
+}
+public class Inheritence {
+    public static void main(String[] args) {
+        Dog d1= new Dog();
+        d1.Walk();
+        d1.getAnimalName();
+
+    }
+}
